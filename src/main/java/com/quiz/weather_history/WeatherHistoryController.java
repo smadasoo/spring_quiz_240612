3f69e4b8252ece2fc44e0dad2199c921fa3694d1
DESKTOP-VS1JCGB\user <test@test.com>
2024-06-24 18:56:57 +0900

    lesson05마무리 종합문제 마크업까지

---
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -0,0 +1,10 @@
+package com.quiz.weather_history;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/weather-hisotry")
+@Controller
+public class WeatherHistoryController {
+
+}
