767fa3a6b8a940ec8ac0b7e50c6f99a1b851d6a5
LAPTOP-4AQS03OK\mucle <an010419@naver.com>
2024-06-25 09:47:44 +0900

    wheather 마크업 수정 더 필요함

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,10 +1,15 @@
 package com.quiz.weather_history;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/weather-hisotry")
+@RequestMapping("/weather-history")
 @Controller
 public class WeatherHistoryController {
 
+	@GetMapping("/add-weather-view")
+	public String add-weather-view(){
+		return weather_history/addWeather;
+	}
 }
