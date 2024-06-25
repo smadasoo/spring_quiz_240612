181e00e9b2d97f4f89f7ca1e184360a9c6f8b700
LAPTOP-4AQS03OK\mucle <an010419@naver.com>
2024-06-26 01:20:30 +0900

    weather문제

--- src/main/java/com/quiz/weather_history/WeatherHistoryController.java
+++ src/main/java/com/quiz/weather_history/WeatherHistoryController.java
@@ -1,24 +1,33 @@
 package com.quiz.weather_history;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.weather_history.bo.WeatherHistoryBO;
+import com.quiz.weather_history.domain.WeatherHistory;
 
 @RequestMapping("/weather-history")
 @Controller
 public class WeatherHistoryController {
 
 	@Autowired
-	private WeatherHistoryBO weatherHistotyBO;
+	private WeatherHistoryBO weatherHistoryBO;
 	
-//	controller가 가져오지 않음 bo가 DB를 가져온다
 	// 날씨 목록 화면 /weather-history/weather-list-view
 	@GetMapping("/weather-list-view")
-	public String weatherListView(Model model){
-		// selelct db
+	public String weatherListView(Model model) {
+		// select db
+		List<WeatherHistory> weatherHistoryList = weatherHistoryBO.getWeatherHistoryList();
 		
-		// model담기
+		// model 담기
+		// model에 담기만 하면 html에서 나오지 않으므로 담는 역활까지 해주어야함
+		model.addAttribute("weatherHistoryList", weatherHistoryList);
 		
 		// 화면 이동
 		return "weather_history/weatherList";
@@ -29,12 +38,16 @@ public class WeatherHistoryController {
 	public String addWeatherView() {
 		return "weather_history/addWeather";
 	}
-	// 날씨를 보내는 것이 아닌 보여주기만 하는것임
-	// 날씨 추가 기능 / weather-history/add-weather
-	@postMapping("/add-weather")
-	public String addWeather(
-			@RequestParam("data") 타입) {
+	
+	// 날씨 추가 기능 /weather-history/add-weather
+//	@PostMapping("/add-weather")
+//	public String addWeather(
+//			@RequestParam("date") 타입 ,
+//			@RequestParam("date") 타입 ,
+//			@RequestParam("date") 타입 
+//			@RequestParam("date") 타입 
+//			@RequestParam("date") 타입 
+//			@RequestParam("date") 타입 ) {
 		
 	}
-}
-
+//}
